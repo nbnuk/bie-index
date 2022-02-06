a9355b7711621c5962b3b6fcf0577ef27a470b0b
Helen Manders Jones <realhelenmjones@gmail.com>
2022-02-06 21:03:48 +0000

    Added speciesCount to result

--- grails-app/services/au/org/ala/bie/SearchService.groovy
+++ grails-app/services/au/org/ala/bie/SearchService.groovy
@@ -1225,6 +1225,9 @@ class SearchService {
                         description     : it.description,
                         occurrenceCount : it.occurrenceCount
                 ]
+                if (it.speciesCount) {
+                    doc.put("speciesCount", it.speciesCount)
+                }
                 if (it.taxonGuid) {
                     doc.put("taxonGuid", it.taxonGuid)
                 }
